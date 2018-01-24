4ad05eb4c0622a0160ed763b16e539f5189c9bbb
Lauren Gray <lauren98gray@gmail.com>
2018-01-23 20:47:40 -0600

    Story ID: #153893058 create welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("\n\nHello, AttendanceApp!\n");
+
+        // Welcome message
+        System.out.println("Welcome!");
     }
 }
