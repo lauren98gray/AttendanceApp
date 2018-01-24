1b2671f1dbec6847cb18e4633ba31b7e5d97f17c
Lauren Gray <lauren98gray@gmail.com>
2018-01-23 20:51:01 -0600

    Story ID: #153805935 Customize welcome message

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,17 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("\n\nHello, AttendanceApp!\n");
 
-        // Welcome message
-        System.out.println("Welcome!");
+        // Customized welcome message
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = sc.nextLine();
+        System.out.println("Welcome, " + name + "!");
+
+
     }
 }
