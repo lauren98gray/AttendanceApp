1b5ba09d33a587762a550ebbca18907f7585368c
Lauren Gray <lauren98gray@gmail.com>
2018-01-23 21:00:04 -0600

    Story ID: #153805996 Create and output list of absences

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
@@ -11,6 +13,15 @@ public class Main {
         System.out.print("Enter your name: ");
         String name = sc.nextLine();
         System.out.println("Welcome, " + name + "!");
+
+        // Make ArrayList of absences
+        ArrayList<Integer> absences = new ArrayList<>();
+        Random rand = new Random();
+        for (int i=0; i < name.length(); i++){
+            int num = rand.nextInt(11);
+            absences.add(num);
+        }
+        System.out.println("The elements are " + absences);
 
 
     }
