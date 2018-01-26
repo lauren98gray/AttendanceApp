1a5cceaf26acf6655a4163ccf0b72d6f73db475b
Lauren Gray <lauren98gray@gmail.com>
2018-01-26 06:20:47 -0600

    Story ID: ##153902131 Create functions to calculate what percentage of students who had fewer than 3 absences also had perfect attendance.

--- src/Main.java
+++ src/Main.java
@@ -23,24 +23,46 @@ public class Main {
         }
         System.out.println("The elements are " + absences);
 
+
         // Number of students with perfect attendance
-        int perfAttend = 0;
-        for (int i=0; i < absences.size(); i++){
-            if (absences.get(i) == 0) {
-                perfAttend++;
-            }
-        }
-        System.out.println(perfAttend + " students had perfect attendance.");
+        System.out.println(perfAttend(absences) + " student(s) had perfect attendance.");
+
 
         // Average of all absences
         System.out.println("The average of all the absences is " + average(absences) + ".");
 
 
+        // % of students with fewer than 3 absences & perfect attendance
+        System.out.println(percent(absences) + "% of students who had fewer than 3 absences also had perfect attendance.");
 
 
 
     }
 
+    // function that counts how many students have perfect attendance
+    private static double perfAttend(ArrayList<Integer> absences) {
+        int perfAttend = 0;
+        for (int i = 0; i < absences.size(); i++) {
+            if (absences.get(i) == 0) {
+                perfAttend++;
+            }
+        }
+        return (double) perfAttend;
+    }
+
+    // function that calculates percent of students that have less than 3 absences and perfect attendance
+    private static double percent(ArrayList<Integer> absences) {
+        int lessThan3 = 0;
+        for (int i=0; i < absences.size(); i++) {
+            if (absences.get(i) < 3) {
+                lessThan3++;
+            }
+        }
+        double percent = (perfAttend(absences) / lessThan3) * 100;
+        return percent;
+    }
+
+    // function that calculates sum
     private static int sum(ArrayList<Integer> absences) {
         int sum = 0;
         for (int i=0; i < absences.size(); i++){
@@ -49,6 +71,7 @@ public class Main {
         return sum;
     }
 
+    // function that calculates average
     private static double average(ArrayList<Integer> absences) {
         int avg = sum(absences) / absences.size();
         return avg;
